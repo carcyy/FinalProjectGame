a0e08ab09a1465dc9d67dc7a980b21670eff6098
Carson H <68257194+carcyy@users.noreply.github.com>
2023-10-26 13:23:05 -0600

    Initial Commit

---
+++ FinalProjectGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinalProjectGame
+//
+//  Created by CUBS Customer on 10/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinalProjectGame/FinalProjectGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectGameApp.swift
+//  FinalProjectGame
+//
+//  Created by CUBS Customer on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
